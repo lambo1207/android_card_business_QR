e42b1d9826fb091c859a65436a4f69611704f7b6
Hoàng Nguyên Bách <hoangnguyenbach2002@gmail.com>
2023-08-09 13:22:10 +0700

    Complete Intro page

---
+++ app/src/main/java/com/example/cardzap/Pages/IntroPage.kt
@@ -0,0 +1,13 @@
+package com.example.cardzap.Pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cardzap.R
+
+class IntroPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_page)
+
+    }
+}
