b8277c5776670c410bbeba0e7288e4291c14f7ea
Hoàng Nguyên Bách <hoangnguyenbach2002@gmail.com>
2023-08-11 16:49:09 +0700

    Connect to Firebase and login with google

--- app/src/main/java/com/example/cardzap/Pages/LoginActivity.kt
+++ app/src/main/java/com/example/cardzap/Pages/LoginActivity.kt
@@ -1,12 +1,99 @@
 package com.example.cardzap.Pages
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cardzap.MainActivity
 import com.example.cardzap.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+
+    private val TAG: String ="LoginPage"
+    private val RC_SIGN_IN:Int = 9001
+    private var mAuth: FirebaseAuth? = null
+
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        //Configuration for google sign in
+        //Start config
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        mAuth =FirebaseAuth.getInstance()
+
+        findViewById<SignInButton>(R.id.btnSignIn).setOnClickListener { signIn() }
     }
+
+    override fun onStart() {
+        super.onStart()
+        //Check if user is signed in (non-null) and update UI accordingly
+        val currentUser = mAuth!!.currentUser
+        updateUI(currentUser)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                //Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                Log.d(TAG, "firebaseAuthWithGoogle" + account.id)
+                account.idToken?.let { firebaseAuthWithGoogle(it) }
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in faled", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    //Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = mAuth!!.currentUser
+                    updateUI(user)
+                } else {
+                    //If sign in fails, display a message to the user
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun signIn(){
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
 }
